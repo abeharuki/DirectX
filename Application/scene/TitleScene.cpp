d66193085261ea47ba186089abbabd9aa45d6a1f
haruki <haruki@MSI>
2023-12-04 18:01:48 +0900

    audioクラス完成

--- Application/scene/TitleScene.cpp
+++ Application/scene/TitleScene.cpp
@@ -1,9 +1,13 @@
 #include "TitleScene.h"
 
-void TitleScene::Initialize() { ; }
+void TitleScene::Initialize() { 
+	audio_ = Audio::GetInstance();
+	audioData_ = audio_->SoundLoadWave("resources/fanfare.wav");
+	audio_->SoundPlayWave(audioData_, true, 3.0f);
+}
 
 void TitleScene::Update() {
-
+	
 	if (KeyInput::PushKey(DIK_P)) {
 		sceneNo_ = GAME;
 	}
--- Application/scene/TitleScene.h
+++ Application/scene/TitleScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IScene.h"
 #include <memory>
+#include "Audio/Audio.h"
 
 class TitleScene : public IScene {
 public:
@@ -9,5 +10,6 @@ public:
 	void Draw() override;
 
 private:
-	
+	Audio* audio_ = nullptr;
+	uint32_t audioData_;
 };
