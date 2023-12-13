931599acab8ec855697e00ec3aad4843176ffc8e
haruki <haruki@MSI>
2023-12-13 10:21:37 +0900

    a

--- Engine/3d/ViewProjection.cpp
+++ Engine/3d/ViewProjection.cpp
@@ -20,6 +20,7 @@ void ViewProjection::Map() {
 void ViewProjection::UpdateMatrix() {
 	UpdateViewMatrix();
 	UpdateProjectionMatrix();
+	TransferMatrix();
 }
 
 void ViewProjection::TransferMatrix() {
