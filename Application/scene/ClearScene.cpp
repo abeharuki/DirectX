8bd7619a9dc42b5d62c14f0ebf276e652e720834
haruki <haruki@MSI>
2024-01-09 21:47:04 +0900

    操作説明

--- Application/scene/ClearScene.cpp
+++ Application/scene/ClearScene.cpp
@@ -4,6 +4,12 @@ void ClearScene::Initialize() {}
 
 void ClearScene::Update() {
 
+	if (KeyInput::GetPadConnect()) {
+		if (KeyInput::GetPadButtonDown(XINPUT_GAMEPAD_B)) {
+			sceneNo_ = TITLE;
+		}
+	}
+
 	if (KeyInput::PushKey(DIK_P)) {
 		sceneNo_ = TITLE;
 	}
--- Application/scene/TitleScene.cpp
+++ Application/scene/TitleScene.cpp
@@ -4,6 +4,12 @@ void TitleScene::Initialize() { ; }
 
 void TitleScene::Update() {
 
+	if (KeyInput::GetPadConnect()) {
+		if (KeyInput::GetPadButtonDown(XINPUT_GAMEPAD_B)) {
+			sceneNo_ = GAME;
+		}
+	}
+
 	if (KeyInput::PushKey(DIK_P)) {
 		sceneNo_ = GAME;
 	}
