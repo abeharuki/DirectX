e6df546e7648f62734fd21b801754d7547022b41
haruki <haruki@MSI>
2024-04-08 22:58:03 +0900

    assimp完成

--- project/Engine/Manager/TextureManeger.cpp
+++ project/Engine/Manager/TextureManeger.cpp
@@ -10,6 +10,9 @@
 #include <fstream>
 #include <sstream>
 #include <filesystem>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 #pragma comment(lib, "dxcompiler.lib")
 
@@ -220,7 +223,7 @@ MaterialData TextureManager::LoadMaterialTemplateFile(const std::string& filenam
 
 
 ModelData TextureManager::LoadObjFile(const std::string& filename) {
-	// 必要な変数の宣言
+	/*/ 必要な変数の宣言
 	ModelData modelData;            // 構築するModelData
 	std::vector<Vector4> positions; // 位置
 	std::vector<Vector3> normals;   // 法線
@@ -297,6 +300,50 @@ ModelData TextureManager::LoadObjFile(const std::string& filename) {
 		}
 	}
 	return modelData;
+	*/
+	
+	ModelData modelData;
+	Assimp::Importer importer;
+	std::string filePath = filename;
+	const aiScene* scene = importer.ReadFile(filePath.c_str(), aiProcess_FlipWindingOrder | aiProcess_FlipUVs);
+	assert(scene->HasMeshes());
+
+
+	for (uint32_t meshIndex = 0; meshIndex < scene->mNumMeshes; ++meshIndex) {
+		aiMesh* mesh = scene->mMeshes[meshIndex];
+		assert(mesh->HasNormals());
+		assert(mesh->HasTextureCoords(0));
+		for (uint32_t faceIndex = 0; faceIndex < mesh->mNumFaces; ++faceIndex) {
+			aiFace& face = mesh->mFaces[faceIndex];
+			assert(face.mNumIndices == 3);
+			for (uint32_t element = 0; element < face.mNumIndices; ++element) {
+				uint32_t vertexIndex = face.mIndices[element];
+				aiVector3D& position = mesh->mVertices[vertexIndex];
+				aiVector3D& normal = mesh->mNormals[vertexIndex];
+				aiVector3D& texcoord = mesh->mTextureCoords[0][vertexIndex];
+				VertexData vertex;
+				vertex.position = { position.x,position.y,position.z,1.0f };
+				vertex.normal = { normal.x,normal.y,normal.z };
+				vertex.texcoord = { texcoord.x,texcoord.y };
+				vertex.position.z *= -1.0f;
+				vertex.normal.x *= -1.0f;
+				modelData.vertices.push_back(vertex);
+			}
+
+		}
+	}
+
+	for (uint32_t materialIndex = 0; materialIndex < scene->mNumMaterials; ++materialIndex) {
+		aiMaterial* material = scene->mMaterials[materialIndex];
+		if (material->GetTextureCount(aiTextureType_DIFFUSE) != 0) {
+			aiString textureFilePath;
+			material->GetTexture(aiTextureType_DIFFUSE, 0, &textureFilePath);
+			modelData.material.textureFilePath = textureFilePath.C_Str();
+		}
+	}
+
+	return modelData;
+	
 }
 
 
