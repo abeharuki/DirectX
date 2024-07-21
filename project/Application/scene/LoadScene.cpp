f464e320dfeb7d851cc05e6520ca62a4b049a9d6
haruki <haruki@MSI>
2024-07-21 13:09:35 +0900

    ロード画面の修正

--- project/Application/scene/LoadScene.cpp
+++ project/Application/scene/LoadScene.cpp
@@ -6,15 +6,18 @@ void LoadScene::Initialize() {
 	backSprite_.reset(Sprite::Create("resources/Black.png"));
 	nowLoadingSprite_.reset(Sprite::Create("resources/load/NowLoading.png"));
 	loadSprite_.reset(Sprite::Create("resources/load/LoadSprite.png"));
-	backSprite_->SetSize({ 1280.0f,800.0f });
+	nowLoadingSprite_->SetAnchorPoint({ 0.5f, 0.5f });
+	nowLoadingSprite_->SetSize(Vector2(1280.0f, 720.0f));
+	backSprite_->SetSize({ 1290.0f,720.0f });
 	loadSprite_->SetAnchorPoint({ 0.5f, 0.5f });
 	loadSprite_->SetPosition({ 1280.0f / 2.0f, 720.0f / 2.0f });
+	loadSprite_->SetSize(Vector2(100.0f, 100.0f));
 	//loadSprite_.reset(Sprite::Create("resources/uvChecker.png"));
 	uv.scale = { 0.0f, 0.0f, 0.0f };
 	uv.rotate = { 0.0f, 0.0f, 0.0f };
 	uv.translate = { 0.0f, 0.0f };
 	spritePos_.x = 0.0f;
-	loadSprite_->SetSize(Vector2(100.0f, 100.0f));
+	
 }
 
 void LoadScene::Update() {
