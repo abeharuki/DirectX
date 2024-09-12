f9a63160e60265a277ea352d810486ed84b9aea5
haruki <haruki@MSI>
2024-09-12 14:52:47 +0900

    敵の体の押し出し処理完成

--- project/Application/objectos/Enemy/DebugEnemy.cpp
+++ project/Application/objectos/Enemy/DebugEnemy.cpp
@@ -3,77 +3,84 @@
 #include <CollisionManager/CollisionConfig.h>
 
 void DebugEnemy::Initialize() {
-
-	
 	transformBase_.scale = { 280.0f,212.0f,1.0f };
 	transformBase_.translate.x = 30.0f;
 	transformBase_.translate.y = 123.0f;
+	transformCore_.scale = { 20.0f,20.0f,0.0f};
+	coreSub_ = { 57.0f,92.0f };
 	velocity_.x = 10.0f;
 	enemySprite_.reset(Sprite::Create("resources/Enemy/boss_kari.png"));
 	enemySprite_->SetAnchorPoint({ 0.25f,0.0f });
+	hitBody_ = false;
+	hitCore_ = false;
 }
 
 void DebugEnemy::Update() {
-
+	preHitBody_ = hitBody_;
+	hitBody_ = false;
+	preHitCore_ = hitCore_;
+	hitCore_ = false;
 	
-	//if (behaviorRequest_) {
-	//	// 振る舞い変更
-	//	behavior_ = behaviorRequest_.value();
-	//	// 各振る舞いごとの初期化
-	//	switch (behavior_) {
-	//	case Behavior::kRoot:
-	//	default:
-	//		MoveInitialize();
-	//		break;
-	//	case Behavior::kJump:
-	//		JumpInitialize();
-	//		break;
-	//	case Behavior::kDash:
-	//		DashInitialize();
-	//		break;
-	//	case Behavior::kAttack:
-	//		AttackInitialize();
-	//		break;
+	if (behaviorRequest_) {
+		// 振る舞い変更
+		behavior_ = behaviorRequest_.value();
+		// 各振る舞いごとの初期化
+		switch (behavior_) {
+		case Behavior::kRoot:
+		default:
+			MoveInitialize();
+			break;
+		case Behavior::kJump:
+			JumpInitialize();
+			break;
+		case Behavior::kDash:
+			DashInitialize();
+			break;
+		case Behavior::kAttack:
+			AttackInitialize();
+			break;
 
-	//	}
+		}
 
-	//	// 振る舞いリセット
-	//	behaviorRequest_ = std::nullopt;
-	//}
+		// 振る舞いリセット
+		behaviorRequest_ = std::nullopt;
+	}
 
-	//switch (behavior_) {
-	//case Behavior::kRoot:
-	//default:
-	//	// 通常行動
-	//	MoveUpdata();
-	//	break;
-	//case Behavior::kJump:
-	//	//ジャンプ
-	//	JumpUpdata();
-	//	break;
-	//case Behavior::kDash:
-	//	// ジャンプ
-	//	DashUpdata();
-	//	break;
-	//case Behavior::kAttack:
-	//	// 攻撃
-	//	AttackUpdata();
-	//	break;
+	switch (behavior_) {
+	case Behavior::kRoot:
+	default:
+		// 通常行動
+		MoveUpdata();
+		break;
+	case Behavior::kJump:
+		//ジャンプ
+		JumpUpdata();
+		break;
+	case Behavior::kDash:
+		// ジャンプ
+		DashUpdata();
+		break;
+	case Behavior::kAttack:
+		// 攻撃
+		AttackUpdata();
+		break;
 
-	//}
+	}
 
-	//if (transformBase_.translate.x <= -30) {
-	//	//velocity_.x = 10.0f;
-	//	velocity_.x = 0.0f;
-	//}
+	for (int i = 0; i < 16; ++i) {
+		ResolveBoxCollision(transformBase_.translate.x - 42, transformBase_.translate.y - 10, transformBase_.scale.x * 0.8f, transformBase_.scale.y,
+			transformBarr_[i].translate.x, transformBarr_[i].translate.y, transformBarr_[i].scale.x, transformBarr_[i].scale.y);
+	}
 
-	//if (transformBase_.translate.x >= 820) {
-	//	velocity_.x = 0.0f;
-	//	//velocity_.x = -10.0f;
-	//}
+	if (transformBase_.translate.x >= 930|| transformBase_.translate.x <= -30) {
+		velocity_.x = 0.0f;
+		//velocity_.x = -10.0f;
+	}
 
 	enemySprite_->SetTransform(transformBase_);
 	Vector3 playerPos = debugPlayer_->GetWorldPosition();
+	transformCore_.translate = (transformBase_.translate + Vector3{ coreSub_.x,coreSub_.y,0.0f});
+
 
 	ImGui::Begin("Enemy");
 	ImGui::Text("posX%f", transformBase_.translate.x);
@@ -82,6 +89,9 @@ void DebugEnemy::Update() {
 	ImGui::DragFloat3("Pos", &transformBase_.translate.x, 0.1f);
 	ImGui::DragFloat3("playerPos", &playerPos.x, 0.1f);
 	ImGui::DragFloat3("velocity" ,&velocity_.x, 0.1f);
+	ImGui::DragFloat3("CoreTranslate", &transformCore_.translate.x, 0.1f);
+	ImGui::DragFloat2("CoreSub", &coreSub_.x, 0.1f);
+	ImGui::DragFloat3("CoreScale", &transformCore_.scale.x, 0.1f);
 	ImGui::End();
 }
 
@@ -91,8 +101,6 @@ void DebugEnemy::Draw(const ViewProjection& camera) {
 
 // 移動
 void DebugEnemy::MoveInitialize() {
-	behaviorJumpTime = 10;
-	behaviorAttackTime = 10;
 
 	//velocity_.y = 0.0f;
 	transformBase_.translate.y = 123.0f;
@@ -106,21 +114,46 @@ void DebugEnemy::MoveUpdata() {
 	}
 
 	//playerの位置が敵から±2の位置にいるかどうか
-	if (!Math::isWithinRange(debugPlayer_->GetWorldPosition().x-60, transformBase_.translate.x, 5.0f)) {
+	if (!Math::isWithinRange(debugPlayer_->GetWorldPosition().x, transformBase_.translate.x+80, 80.0f)) {
 		
 
 		//playerと敵の距離が5離れたいたらジャンプして移動
 		if (debugPlayer_->GetWorldPosition().x-60 < transformBase_.translate.x) {
-			velocity_.x = -10.0f;
+			for (int i = 0; i < 16; ++i) {
+				if (Math::IsBoxCollision(transformBase_.translate.x - 52, transformBase_.translate.y - 10, transformBase_.scale.x * 0.8f, transformBase_.scale.y,
+					transformBarr_[i].translate.x, transformBarr_[i].translate.y, transformBarr_[i].scale.x, transformBarr_[i].scale.y)) {
+					velocity_.x = 0.0f;
+					break;
+				}
+				else {
+					velocity_.x = -10.0f;
+				}
+			}
+		
+			
 		}
 		else {
-			velocity_.x = 10.0f;
+			for (int i = 0; i < 16; ++i) {
+				if (Math::IsBoxCollision(transformBase_.translate.x - 32, transformBase_.translate.y - 10, transformBase_.scale.x * 0.8f, transformBase_.scale.y,
+					transformBarr_[i].translate.x, transformBarr_[i].translate.y, transformBarr_[i].scale.x, transformBarr_[i].scale.y)) {
+					velocity_.x = 0.0f;
+					break;
+				}
+				else {
+					velocity_.x = 10.0f;
+				}
+			}
+			
 		}
 
-		/*if (--behaviorJumpTime <= 0) {
-			
+		if (velocity_.x != 0) {
+			--behaviorJumpTime;
+		}
+		if (behaviorJumpTime <= 0 ) {
 			behaviorRequest_ = Behavior::kJump;
-		}*/
+		}
+
+		
 	}
 	else {
 		//プレイヤーの上にいる時,速度はゼロ
@@ -132,7 +165,20 @@ void DebugEnemy::MoveUpdata() {
 		
 	}
 
+	if (hitBody_ != preHitBody_) {
+		for (int i = 0; i < 16; ++i) {
+			ResolveBoxCollision(transformBase_.translate.x - 42, transformBase_.translate.y - 10, transformBase_.scale.x * 0.8f, transformBase_.scale.y,
+				transformBarr_[i].translate.x, transformBarr_[i].translate.y, transformBarr_[i].scale.x, transformBarr_[i].scale.y);
+		}
+
+	}
+
+	
+
 	transformBase_.translate += velocity_;
+
+	
+	
 	//if (--behaviorAttackTime <= 0) {
 	//	behaviorRequest_ = Behavior::kAttack;
 	//}
@@ -141,44 +187,50 @@ void DebugEnemy::MoveUpdata() {
 
 // ジャンプ
 void DebugEnemy::JumpInitialize() {
-	transformBase_.translate.y = 5.0f;
+	transformBase_.translate.y = 123.0f;
 	// ジャンプ初速
-	const float kJumpFirstSpeed = 5.0f;
+	const float kJumpFirstSpeed = -10.0f;
 	velocity_.y = kJumpFirstSpeed;
 	jump_ = true;
+	behaviorJumpTime = 10;
 };
 void DebugEnemy::JumpUpdata() {
+
+	
 
 	// 移動
 	transformBase_.translate += velocity_;
 	// 重力加速度
-	const float kGravity = 0.03f;
+	const float kGravity = 0.5f;
 	// 加速ベクトル
-	Vector3 accelerationVector = { 0, -kGravity, 0 };
+	Vector3 accelerationVector = { 0, kGravity, 0 };
 	// 加速
 	velocity_ += accelerationVector;
-	// 移動速度
-	const float kCharacterSpeed = 0.2f;
+	if (hitBody_) {
+		velocity_.x = 0.0f;
+	
+	}
 
 	//ジャンプ中にプレイヤーが真下に来たら攻撃
-	if (Math::isWithinRange(debugPlayer_->GetWorldPosition().x,transformBase_.translate.x, 1.0f)) {
-		if (transformBase_.translate.y >= 8.5f) {
-			//プレイヤーの上にいる時,速度はゼロ
-			velocity_.x = 0.0f;
-			//クールタイムが終わったら攻撃
-			behaviorRequest_ = Behavior::kAttack;
-		}
+	//if (Math::isWithinRange(debugPlayer_->GetWorldPosition().x,transformBase_.translate.x, 1.0f)) {
+	//	if (transformBase_.translate.y >= 8.5f) {
+	//		//プレイヤーの上にいる時,速度はゼロ
+	//		velocity_.x = 0.0f;
+	//		//クールタイムが終わったら攻撃
+	//		behaviorRequest_ = Behavior::kAttack;
+	//	}
 
-		
-	}
+	//	
+	//}
+	
 	
 
-	if (transformBase_.translate.y <= 5.0f) {
+	if (transformBase_.translate.y >= 123.0f) {
 		velocity_.y = 0.0f;
-		transformBase_.translate.y = 5.0f;
-
-		// ジャンプ終了
+		transformBase_.translate.y = 123.0f;
+	
 		behaviorRequest_ = Behavior::kRoot;
+
 	}
 };
 
@@ -204,7 +256,7 @@ void DebugEnemy::AttackInitialize() {
 		
 	}
 	
-	
+	behaviorAttackTime = 15;
 	behaviorMoveTime = 20;
 
 }
@@ -240,10 +292,17 @@ void DebugEnemy::AttackUpdata() {
 		
 		// 攻撃終了
 		if (--behaviorMoveTime <= 0) {
+			
 			behaviorRequest_ = Behavior::kRoot;
 			attack_ = false;
 		}
 		
+	}
+}
+
+void DebugEnemy::Damage(){
+	if (hitCore_ != preHitCore_) {
+
 	}
 }
 
--- project/Application/objectos/Enemy/DebugEnemy.h
+++ project/Application/objectos/Enemy/DebugEnemy.h
@@ -50,22 +50,72 @@ public: // メンバ関数
 	void AttackInitialize();
 	void AttackUpdata();
 
+	//攻撃を食らったときの計算
+	void Damage();
 
+	/*ゲッターとセッター*/
 	void SetViewProjection(const ViewProjection* viewProjection) {viewProjection_ = viewProjection;}
 	void SetDebugPlayer(DebugPlayer* debugPlayer) { debugPlayer_ = debugPlayer; }
+	void SetHitBody(bool hit) { hitBody_ = hit; }
+	void SetHitCore(bool hit) { hitCore_ = hit; }
+	void SetBarr(Vector2 pos, Vector2 scale,uint32_t i) { transformBarr_[i].translate = Vector3{pos.x,pos.y,0.0f};  transformBarr_[i].scale = Vector3{scale.x,scale.y,0.0f}; }
 	bool GetAttack() { return attack_; }
 	Vector2 GetPos() { return Vector2{ transformBase_.translate.x,transformBase_.translate.y }; }
 	Vector2 GetScale() {return Vector2{ transformBase_.scale.x,transformBase_.scale.y };}
+	Vector2 GetCorePos() { return Vector2{ transformCore_.translate.x,transformCore_.translate.y }; }
+	Vector2 GetCoreScale() { return Vector2{ transformCore_.scale.x,transformCore_.scale.y }; }
+
+
+	//押し出し関数
+	void ResolveBoxCollision(float AposX, float AposY, const float AsizeW, const float AsizeH,
+		float& BposX, float& BposY, const float BsizeW, const float BsizeH)
+	{
+		// 衝突判定を行う
+		if (Math::IsBoxCollision(AposX, AposY, AsizeW, AsizeH, BposX, BposY, BsizeW, BsizeH)) {
+			// 重なり量の計算
+			float overlapX1 = (AposX + AsizeW) - BposX; // Aが左側からBに重なっている場合の重なり量
+			float overlapX2 = (BposX + BsizeW) - AposX; // Aが右側からBに重なっている場合の重なり量
+			float overlapY1 = (AposY + AsizeH) - BposY; // Aが上側からBに重なっている場合の重なり量
+			float overlapY2 = (BposY + BsizeH) - AposY; // Aが下側からBに重なっている場合の重なり量
+
+			// 最小の重なりを見つけ、その方向に押し出す
+			if (overlapX1 < overlapX2 && overlapX1 < overlapY1 && overlapX1 < overlapY2) {
+				// Aを左側に押し出す
+				transformBase_.translate.x -= overlapX1;
+				if (velocity_.x >= 1) {
+					velocity_.x = 0;
+				}
+			}
+			else if (overlapX2 < overlapY1 && overlapX2 < overlapY2) {
+				// Aを右側に押し出す
+				transformBase_.translate.x += overlapX2;
+				if (velocity_.x <= 1) {
+					velocity_.x = 0;
+				}
+			}
+
+			if (overlapY1 < overlapY2) {
+				// Aを上側に押し出す
+				//preHitBody_ = hitBody_;
+				//transformBase_.translate.y -= overlapY1;
+			}
+			else {
+			
+			}
+		}
+		
+	}
+
 
 private: // メンバ変数
 	Transform transformBase_;
+	Transform transformCore_;
+	std::array<Transform,16> transformBarr_;
+	Vector2 coreSub_;
 	const ViewProjection* viewProjection_;
 	std::unique_ptr<Sprite> enemySprite_;
 
 	DebugPlayer* debugPlayer_;
-
-	// 目標の角度
-	float destinationAngleY_ = 0.0f;
 
 	// 振る舞い
 	enum class Behavior {
@@ -81,19 +131,12 @@ private: // メンバ変数
 	// 次の振る舞いリクエスト
 	std::optional<Behavior> behaviorRequest_ = std::nullopt;
 
-	// ダッシュ用ワーク
-	struct WorkDash {
-		// ダッシュ用媒介変数
-		uint32_t dashParameter_ = 0;
-		// 移動速度
-		float dashSpeed = 1.0f;
-		// ダッシュの時間
-		const uint32_t behaviorDashTime = 15;
-	};
-	WorkDash workDash_;
-
 	bool attack_;
 	bool jump_;
+	bool hitBody_;
+	bool preHitBody_;
+	bool hitCore_;
+	bool preHitCore_;
 
 	// ジャンプの時間
 	uint32_t behaviorJumpTime = 30;
