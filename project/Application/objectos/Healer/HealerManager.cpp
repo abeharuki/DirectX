2c0dc1eccd6662f67efac77de4194c0273f0b61d
haruki <haruki@MSI>
2024-11-12 12:43:15 +0900

    healerのステックが表示されないバグの修正

--- project/Application/objectos/Healer/HealerManager.cpp
+++ project/Application/objectos/Healer/HealerManager.cpp
@@ -4,31 +4,27 @@ void HealerManager::Initialize() {
 	animation_ = std::make_unique<Animations>();
 	animation_.reset(Animations::Create("./resources/Healer", "Atlas.png", "healer.gltf"));
 
+
+
+	StaffModel_.reset(Model::CreateModelFromObj("resources/Healer/weapon.obj", "resources/Healer/staff.png"));
+
 	
 	healer_ = std::make_unique<Healer>();
 	healer_->Initialize(animation_.get(),"Healer");
 
-	
 }
 
 void HealerManager::Update() {
-	
-
 	//キャラクターの更新
 	healer_->Update();
-	
 };
 
 void HealerManager::Draw(const ViewProjection& camera) {
 	healer_->Draw(camera);
 	
-	
-
 	if (healer_->IsAttack()) {
 		StaffModel_->Draw(healer_->GetWorldTransformCane(), camera, false);
 	}
 	
 }
-
-
 
